a2e53c83413ad83437a5a80291f902a361fbdf6f
yuanbosdu <yuanbosdu@163.com>
2018-07-08 21:58:10 +0800

    modify

--- src/assets/vue/pages/forum/forum_list.vue
+++ src/assets/vue/pages/forum/forum_list.vue
@@ -1,11 +1,40 @@
 <template>
+  <f7-page>
+    <f7-navbar title="论坛" back-link="Back" sliding></f7-navbar>
+    <f7-block-title>论坛1</f7-block-title>
+    <f7-list media-list>
+      <f7-list-item
+        tile="topic1"
+        subtitle="topic1 description">
+        <img slot="media" src="" width="44" />
+      </f7-list-itme>
+    </f7-list>
+    <f7-block-title>论坛1</f7-block-title>
+    <f7-list media-list>
+      <f7-list-item
+        tile="topic1"
+        subtitle="topic1 description">
+        <img slot="media" src="" width="44" />
+      </f7-list-itme>
+    </f7-list>
+    <f7-block-title>论坛1</f7-block-title>
+    <f7-list media-list>
+      <f7-list-item
+        tile="topic1"
+        subtitle="topic1 description">
+        <img slot="media" src="" width="44" />
+      </f7-list-itme>
+    </f7-list>
+  </f7-page>
 </template>
 <style scoped>
 </style>
 <script>
 export default {
   data() {
+    return {
 
+    }
   },
   computed: {
 
